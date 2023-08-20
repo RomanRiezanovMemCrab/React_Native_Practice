23ed2a84f7b49ce1399ca3a17c36c9a8b8ef3ae4
Roman Riezanov <rriezanov@gmail.com>
2023-08-20 20:41:30 +0300

    Changed name of project to "react_native_app"

--- ios/react_native_appTests/react_native_appTests.m
+++ ios/react_native_appTests/react_native_appTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface react_native_app : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation react_native_app
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
